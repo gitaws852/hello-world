fc6e09fcf0bb7a70d44be6e942083f891d00be31
gitaws852 <92790480+gitaws852@users.noreply.github.com>
2021-10-20 15:10:28 +0530

    otl_310
    otl_310

--- test_otl.py
+++ test_otl.py
@@ -2,7 +2,7 @@ import pytest
 import json
 import otl_cases
 
-clientId = 9ec75914-f886-4dce-9557-9e54efa7c821
+clientId = "9ec75914-f886-4dce-9557-9e54efa7c821"
 headers = {}
 
 # otl sandbox auth intiate call - gives success response
